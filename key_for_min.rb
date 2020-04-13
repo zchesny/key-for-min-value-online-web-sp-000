f7dd55bda9d666812a8462902e2e21b01f85b043
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 16:45:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |key, value|
     if value < min_value
-      min_value = value 
+      min_value = value
       min_key = key
     end
   end
